6b0fee7e9dc86d51afa9ea3c368e0a594a73cfb8
monikabiras <monikabiras@gmail.com>
2023-12-14 00:10:34 +0100

    post

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/entity/Post.java
@@ -0,0 +1,106 @@
+package cz.czechitas.java2webapps.ukol7.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String slug;
+
+    @NotBlank
+    private String author;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String perex;
+
+    @NotBlank
+    private String body;
+
+    private LocalDate published;
+
+    public Post() {
+    }
+
+    public Post(Long id, String slug, String author, String title, String perex, String body, LocalDate published) {
+        this.id = id;
+        this.slug = slug;
+        this.author = author;
+        this.title = title;
+        this.perex = perex;
+        this.body = body;
+        this.published = published;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPerex() {
+        return perex;
+    }
+
+    public void setPerex(String perex) {
+        this.perex = perex;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDate getPublished() {
+        return published;
+    }
+
+    public void setPublished(LocalDate published) {
+        this.published = published;
+    }
+}
