7f0f5c5e0a14b9abd964dffa28078a51f0be3458
monikabiras <monikabiras@gmail.com>
2023-12-14 00:12:54 +0100

    postrep+serv

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package cz.czechitas.java2webapps.ukol7.repository;
+
+import cz.czechitas.java2webapps.ukol7.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByPublishedBeforeOrderByPublished(LocalDate datum, Pageable pageable);
+
+    Post findBySlug(String slug);
+}
---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/service/PostService.java
@@ -0,0 +1,34 @@
+package cz.czechitas.java2webapps.ukol7.service;
+
+import cz.czechitas.java2webapps.ukol7.entity.Post;
+import cz.czechitas.java2webapps.ukol7.repository.PostRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Page<Post> list() {
+        return postRepository.findByPublishedBeforeOrderByPublished(LocalDate.now(), PageRequest.of(0, 20));
+    }
+
+    public Post singlePost(String slug) {
+        return postRepository.findBySlug(slug);
+    }
+}
